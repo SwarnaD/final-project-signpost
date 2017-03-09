d7c5854820a9e87463659f30a39201429015907a
josephmflee <joseph.ho.cheung.lee@gmail.com>
2017-03-08 23:43:54 -0500

    added more functions to app.js

--- app.js
+++ app.js
@@ -41,13 +41,18 @@ app.get('/login', function(req,res) {
 });
 
 //API URLS
-app.post('/login', login.addUser);
-app.post('/clubs', club.addClub)
-app.get('/clubs', clubs.searchClubs);
-app.post('/login', login.signin);
-app.get('/logout', login.signout);
-app.delete('/clubs', clubs.deleteClub);
-app.get('/profile', clubs.addProfile)
+app.post('/login', login.addUser); // Add a user
+app.post('/login', login.signin); // User sign in
+
+app.get('/logout', login.signout); // User sign out
+
+app.post('/clubs', club.addClub); // Add a club
+app.put('/clubs', club.editClub); // Edit club details
+app.get('/clubs', clubs.searchClubs); // Search for clubs
+app.delete('/clubs', clubs.deleteClub); // Delete a club
+
+app.get('/profile', profile.addProfile);  // Add a user profile
+app.get('/profile', profile.editProfile); // Edit profile attributes
 
 app.listen(process.env.PORT || 3000);
 console.log('Listening on port 3000');
