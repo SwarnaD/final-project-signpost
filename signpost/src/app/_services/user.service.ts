aa24a01f15ed16f0e85f801f40b98ec4af7319ac
Justin Mao <justinmao@live.com>
2017-03-12 21:11:16 -0400

    [#141335393] removed misnamed folder

--- signpost/src/app/_services/user.service.ts
+++ signpost/src/app/_services/user.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map'
+
+import { AuthService } from '../_services/auth';
+import { User } from '../_models/user';
 
 @Injectable()
 export class UserService {
 
-  constructor() { }
+  constructor(http: Http, authService: AuthService) { }
+
+  getUsers(): Observable<User[]> {
+    let headers = new Headers({ 'authToken': this.authService.token });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.get('/api/users', options)
+      .map((response: Reponse) => response.json());
+  }
 
 }
