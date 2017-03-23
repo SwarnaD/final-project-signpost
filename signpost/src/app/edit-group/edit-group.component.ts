e213e6b56f90cf85587cfac585a3f78e1b5c4e69
selby gomes <selby gomes>
2017-03-22 20:14:14 -0400

    adding

--- signpost/src/app/edit-group/edit-group.component.ts
+++ signpost/src/app/edit-group/edit-group.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Group } from '../_models/group';
 import { GroupService } from '../_services/group.service'
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-edit-group',
   templateUrl: './edit-group.component.html',
@@ -10,15 +10,34 @@ import { GroupService } from '../_services/group.service'
 })
 export class EditGroupComponent implements OnInit {
   error = '';
+  _id: String;
+  group: any = [];
+  model: Group ={name: '' ,description:'a',campus:'',tags:''}
 
+  
   constructor(
-      private groupService: GroupService,
-      private router: Router) {
-  }
+      private route: ActivatedRoute,
+      private groupService: GroupService) {
+        }
 
   ngOnInit() {
+      this.route.params.subscribe(params => {
+            this._id = params['id'];
+      });
+            this.getGroup();
+	    alert(this.group.name);
+            this.model ={name: this.group.name ,description: this.group.description ,campus: this.group.campus,tags: this.group.tags }
+
+
+
   }
-  /*register() {
+  getGroup() {
+  	console.log(this._id);
+    this.groupService.getGroup(this._id).subscribe(group => {
+      this.group = group;
+    });
+  }
+  /*editGroup() {
       this.groupService.editGroup(this._id, this.model.name, this.model.description, this.model.campus, this.model.tags).subscribe(result => {
       if (result === true) {
           this.router.navigate([/]);
